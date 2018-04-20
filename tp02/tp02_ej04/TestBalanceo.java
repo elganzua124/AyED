470cf053096dffba2907e2413eef64acc6d71f09
Francisco Herrera <fran.herrera124@gmail.com>
2018-04-20 14:06:54 -0300

    ok

--- tp02/tp02_ej04/TestBalanceo.java
+++ tp02/tp02_ej04/TestBalanceo.java
@@ -2,41 +2,49 @@ package tp02_ej04;
 
 import tp02_ej03.PilaGenerica;
 
+import java.util.Scanner;
+
 public class TestBalanceo {
 
 	private static PilaGenerica<Character> pila = new PilaGenerica<Character>();
 
 	public static void main(String[] args) {
-		String prueba = new String();
-		prueba = "([{}({})])";
-		System.out.format("Test balanceo de string: \"%s\"%n", prueba);
-		if (testBalanceo(prueba))
-			System.out.print("Es balanceado.");
-		else
-			System.out.print("No es balanceado.");
+		Scanner sc = new Scanner(System.in);
+		System.out.format("Escriba una cadena %n(escribir 'salir' para salir): ");
+		String cadena = sc.nextLine();
+		while (!cadena.equals("salir")) {
+			if (testBalanceo(cadena))
+				System.out.println("Es balanceado.");
+			else
+				System.out.println("No es balanceado.");
+			System.out.format("Escriba una cadena %n(escribir 'salir' para salir): ");
+			cadena = sc.nextLine();
+		}
 	}
 
 	private static char aperturaDeCierre(char caracter) {
 		if (caracter == ')')
-			caracter = '(';
-		else if (caracter == ']')
-			caracter = '[';
-		else if (caracter == '}')
-			caracter = '{';
-		return caracter;
+			return '(';
+		if (caracter == ']')
+			return '[';
+		if (caracter == '}')
+			return '{';
+		return ' ';
 	}
 
 	private static boolean esApertura(char caracter) {
 		return (caracter == '(' || caracter == '[' || caracter == '{');
 	}
 
-	public static boolean testBalanceo(String cadena) {
+	private static boolean testBalanceo(String cadena) {
 		boolean balanceado = true;
 		int pos = 0;
-		Character ch = cadena.charAt(pos);
-
-		while (balanceado && pos != cadena.length()) {
-
+		Character ch;
+		pila.apilar('\b'); /* para que pila.tope() no tire error
+		porque primer cadena no era balanceada. Evaluar siempre
+		que no es vacia sólo para ese caso es un despropósito.*/
+		while (pos < cadena.length() && balanceado) {
+			ch = cadena.charAt(pos++);
 			if (esApertura(ch)) {
 				pila.apilar(ch);
 			} else {
