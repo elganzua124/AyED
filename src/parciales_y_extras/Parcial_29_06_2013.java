79fbdbd58e4547b59c31e727361cec998193070e
elganzua124 <fran.herrera124@gmail.com>
2020-07-22 00:26:17 -0300

    Agregado enunciado de parcial vejo para hacer

---
+++ src/parciales_y_extras/Parcial_29_06_2013.java
@@ -0,0 +1,27 @@
+package parciales_y_extras;
+
+/**
+ *
+ * ¿Es sólo una moda o vino para quedarse? No se sabe, pero como parte de un
+ * plan fuerte de expansión en Argentina el número de locales abiertos de una
+ * muy conocida cadena internacional de café ha aumentado 30% en la primera
+ * mitad de 2013 con respecto al mismo período del año 2012.
+ * 
+ * Al parecer las personas se han vuelto tan adictas a estas tiendas de café
+ * gourmet que los inmuebles que están cerca de estas cafeterías obtienen
+ * mejores rentas. Esto ha sido notado por una compañía de bienes raíces, que
+ * está interesada en identificar si una determinada esquina es un lugar valioso
+ * en términos de su proximidad al mayor número de cafés de esta cadena.
+ * 
+ * Para ello cuentan con un mapa de la ciudad representada en un grafo, donde
+ * cada arista indica la cantidad de cafés en esa cuadra. Supongamos que una
+ * persona promedio está dispuesta a caminar un número de cuadras fijo X para
+ * obtener su café matinal. Usted tiene que determinar si la esquina en cuestión
+ * es valiosa. Es considerada valiosa si el número de cafés en los que una
+ * persona puede alcanzar es mayor a un número fijo arbitrario Y.
+ * 
+ */
+
+public class Parcial_29_06_2013 {
+	// hacer
+}
