3f22fa0253e339b739874ff1e9481de263387e1a
elganzua124 <fran.herrera124@gmail.com>
2020-08-02 18:54:51 -0300

    hacer

---
+++ src/parciales_y_extras/Ej_25_04_08.java
@@ -0,0 +1,14 @@
+package parciales_y_extras;
+
+/*
+ * Realizando el recorrido DFS sobre un grafo es posible obtener el
+ * bosque abarcador en profundidad. Se pide modificar el algoritmo
+ * DFS para que calcule la cantidad de árboles del bosque. Además,
+ * se debe registrar el número de árbol al que pertenece cada nodo.
+ * Se supone que el primer árbol generado es el 1, luego el 2 y así
+ * sucesivamente.
+ */
+
+public class Ej_25_04_08 {
+//hacer
+}
